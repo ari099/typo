0be874f43a9aef6aa1d7946ce1ed9333970d07d2
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-06-27 13:55:25 -0400

    Initiation

---
+++ app.py
@@ -0,0 +1,8 @@
+import typo
+
+# PROGRAM ENTRY POINT....
+def main():
+	pass
+
+if __name__=='__main__':
+	main()
---
+++ typo.py
@@ -0,0 +1,10 @@
+import curses
+import os
+import sys
+
+class Typo:
+   """
+   Typo object
+   """
+   def __init__(self):
+      pass
