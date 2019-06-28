0bca4b7e3ef061bb38cfafb673ed50823f497237
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-06-28 08:53:34 -0400

    Moved wrapper function code to the Typo class in 'typo.py'

--- app.py
+++ app.py
@@ -1,41 +1,10 @@
 import curses
 import os
 import sys
-
-def typo(stdscr):
-   # Clear the screen
-   stdscr.clear()
-
-   cmd_h, cmd_w = stdscr.getmaxyx()
-   stdscr.addstr(0,0, " TYPO ".center(cmd_w, '-'))
-   stdscr.refresh()
-   if len(sys.argv) > 1: stdscr.addstr(1,0, sys.argv[0].center(cmd_w,' '))
-   else: stdscr.addstr(1,0, " New Buffer ".center(cmd_w, ' '))
-   stdscr.refresh()
-   stdscr.addstr(2,0, '-'*cmd_w)
-   stdscr.refresh()
-
-   x = 0; y = 3
-   h = cmd_h - 3; w = cmd_w
-   win = curses.newwin(cmd_h - 3, cmd_w, 3, 0)
-   win.refresh()
-
-   stdscr.refresh() # Adding the text to the screen
-
-   command_bar = curses.newwin(cmd_h - h, cmd_w, cmd_h - 3, 0)
-   command_bar.addstr(0,0, "^0 = save file")
-   command_bar.addstr(1,0, "^X = exit Typo")
-   command_bar.refresh()
-
-   stdscr.refresh()
-   stdscr.getkey()
+from typo import Typo
 
 def main():
-   # The curses.wrapper() function takes a callable object and does the
-   # necessary initializations. curses.wrapper() then runs your provided callable
-   # Once the callable returns, curses.wrapper() will restore the
-   # original state of the terminal
-   curses.wrapper(typo)
+   Typo()
 
 if __name__ == "__main__":
    main()
--- typo.py
+++ typo.py
@@ -7,13 +7,32 @@ class Typo:
    Typo object
    """
    def __init__(self):
-      self.screen = curses.initscr()
-      self.height, self.width = self.screen.getmaxyx()
-      self.screen.addstr("*"*self.width)
-      self.screen.refresh()
-      end_character = ""
-      while '^C' not in end_character:
-         self.screen.getch()
+      curses.wrapper(self.entry_point)
    
-   def __del__(self):
-      curses.endwin()
+   def entry_point(self, stdscr):
+      # Clear the screen
+      stdscr.clear()
+
+      cmd_h, cmd_w = stdscr.getmaxyx()
+      stdscr.addstr(0,0, " TYPO ".center(cmd_w, '-'))
+      stdscr.refresh()
+      if len(sys.argv) > 1: stdscr.addstr(1,0, sys.argv[0].center(cmd_w,' '))
+      else: stdscr.addstr(1,0, " New Buffer ".center(cmd_w, ' '))
+      stdscr.refresh()
+      stdscr.addstr(2,0, '-'*cmd_w)
+      stdscr.refresh()
+
+      x = 0; y = 3
+      h = cmd_h - 3; w = cmd_w
+      win = curses.newwin(cmd_h - 3, cmd_w, 3, 0)
+      win.refresh()
+
+      stdscr.refresh() # Adding the text to the screen
+
+      command_bar = curses.newwin(cmd_h - h, cmd_w, cmd_h - 3, 0)
+      command_bar.addstr(0,0, "^0 = save file")
+      command_bar.addstr(1,0, "^X = exit Typo")
+      command_bar.refresh()
+
+      stdscr.refresh()
+      stdscr.getkey()
